2249a96efbd1f46cadfde8f597d8b18d22acc2de
LameckASC6 <52749282+LameckASC6@users.noreply.github.com>
2019-07-16 21:29:39 -0400

    Update blackJack.js
    To start playing all you have to do is go on terminal/cmd and node the file; afterwards the game would start asking you prompts answer them; the goal of this game is to reach 21. It is getting a random card which you would use afterwards in the game that is within the array yourCards; In the end it adds up the values of yourCards bassed on the typical rules of blackjack; afterwards that answer determins whether you win or not.

--- blackJack.js
+++ blackJack.js
@@ -1,6 +1,7 @@
 let yourCards = [];
 let yourCardsCheck = [];
 let results;
+
 function getCards(n){
     for (i = 0; i < n; i ++){
         let cards = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
@@ -11,6 +12,7 @@ function getCards(n){
         yourCardsCheck.splice(yourCardsCheck.length, 0, yCardsCheck);
         yourCards.splice(yourCards.length, 0, yCards);
     }
+    
     results = yourCardsCheck.reduce((a, b) => a + b, 0)
     if (results == 21){
         console.log("You Won");
@@ -28,6 +30,7 @@ console.log("Whats your name?");
 let name = require('prompt');
 name.start();
 name.get(['name'], function (err, result){
+    
     console.log("Good evening " + result.name + ", Are you ready to play?");
     let userName = result.name;
     let game = require('prompt');
@@ -35,13 +38,16 @@ name.get(['name'], function (err, result){
     game.get(['answer'], function (err, result){
     console.log("Great " + userName + ", here are your cards.");
     console.log(getCards(2));
+        
         console.log(userName + ", do you want to hit or stay?");
         let gameHit = require('prompt');
         gameHit.start();
         gameHit.get(['goOn'], function(err, result){
+            
             let goOn = result.goOn;
             if (result.goOn == "stay"){
                 console.log(yourCards);
+                
                 if (result == 21){
                     console.log("You Won");
                 } else if (results > 21){
@@ -50,13 +56,15 @@ name.get(['name'], function (err, result){
                     console.log("Close, But Not Good Enough");
                 }
                 return; 
-            } else if (result.goOn == "hit"){
+                } else if (result.goOn == "hit"){
+                    
                     console.log(getCards(1));
                     for (let i = 0; i < 1; i++){
                         console.log (userName + ", do you want to 'hit' again, or stay?");
                         let gameAgain = require('prompt')
                         gameAgain.start()
                         gameAgain.get(['again'], function (err, result){
+                            
                             if (result.again == "stay"){
                                 console.log(yourCards);
                                 goOn = "stay";
@@ -68,14 +76,15 @@ name.get(['name'], function (err, result){
                                     console.log("Close, But Not Good Enough");
                                 }
                                 return;
-                            } else if (result.again == "hit"){                                   
+                                
+                                } else if (result.again == "hit"){                                   
                                     console.log(getCards(1));
                                     goOn = "hit";
-                            } else {
-                                console.log("Please, reply with 'stay' or 'hit', and do not use other terminology");
-                            }
-                            });
-                    }
+                                } else {
+                                    console.log("Please, reply with 'stay' or 'hit', and do not use other terminology");
+                                    }
+                                });
+                        }
                 } else {
                         console.log("Please, reply with 'stay' or 'hit', and do not use other terminology");
                     }
